f234b7138ef7c0e7dc16d98f885dcedfc5bb0edf
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-06-08 10:10:06 -0700

    popup fetch and input test

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
+
+  function logInp() {
+    const inp = document.getElementById('inp_box')
+    console.log('test')
+    console.log(inp.value)
+
+    fetch('https://raw.githubusercontent.com/dgca/relay-hasura-demo/main/src/App.tsx')
+      .then(data => data.text())
+      .then(res => console.log(res))
+      .catch(err => console.error(err))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/pages/Popup/Popup.js</code> and save to reload.
+          EDIT <code>src/pages/Popup/Popup.js</code> and save to reload.
         </p>
+        <input id="inp_box" />
+        <button onClick={logInp}>Click sub</button>
         <a
           className="App-link"
           href="https://reactjs.org"
